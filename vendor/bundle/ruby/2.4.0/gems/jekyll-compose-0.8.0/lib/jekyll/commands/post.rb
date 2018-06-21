2250c5a9d1598947034969edf47b76ace2c6988f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-21 02:13:42 +0000

    Deploy NickTomlin/nicktomlin.github.io to github.com/NickTomlin/nicktomlin.github.io.git:master

---
+++ vendor/bundle/ruby/2.4.0/gems/jekyll-compose-0.8.0/lib/jekyll/commands/post.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Commands
+    class Post < Command
+      def self.init_with_program(prog)
+        prog.command(:post) do |c|
+          c.syntax "post NAME"
+          c.description "Creates a new post with the given NAME"
+
+          options.each { |opt| c.option *opt }
+
+          c.action { |args, options| process args, options }
+        end
+      end
+
+      def self.options
+        [
+          ["extension", "-x EXTENSION", "--extension EXTENSION", "Specify the file extension"],
+          ["layout", "-l LAYOUT", "--layout LAYOUT", "Specify the post layout"],
+          ["force", "-f", "--force", "Overwrite a post if it already exists"],
+          ["date", "-d DATE", "--date DATE", "Specify the post date"],
+          ["config", "--config CONFIG_FILE[,CONFIG_FILE2,...]", Array, "Custom configuration file"],
+          ["source", "-s", "--source SOURCE", "Custom source directory"],
+        ]
+      end
+
+      def self.process(args = [], options = {})
+        params = PostArgParser.new args, options
+        params.validate!
+
+        post = PostFileInfo.new params
+
+        file_creator = Compose::FileCreator.new(post, params.force?, params.source)
+        file_creator.create!
+        Compose::FileEditor.open_editor(file_creator.file_path)
+      end
+
+      class PostArgParser < Compose::ArgParser
+        def date
+          options["date"].nil? ? Time.now : Date.parse(options["date"])
+        end
+      end
+
+      class PostFileInfo < Compose::FileInfo
+        def resource_type
+          "post"
+        end
+
+        def path
+          "_posts/#{file_name}"
+        end
+
+        def file_name
+          "#{_date_stamp}-#{super}"
+        end
+
+        def _date_stamp
+          @params.date.strftime "%Y-%m-%d"
+        end
+
+        def _time_stamp
+          @params.date.strftime("%Y-%m-%d %H:%M %z")
+        end
+
+        def content(custom_front_matter = {})
+          super({ "date" => _time_stamp }.merge(custom_front_matter))
+        end
+      end
+    end
+  end
+end
---
+++ vendor/bundle/ruby/2.4.0/gems/jekyll-compose-0.8.0/lib/jekyll/commands/publish.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Commands
+    class Publish < Command
+      def self.init_with_program(prog)
+        prog.command(:publish) do |c|
+          c.syntax "publish DRAFT_PATH"
+          c.description "Moves a draft into the _posts directory and sets the date"
+
+          c.option "date", "-d DATE", "--date DATE", "Specify the post date"
+          c.option "config", "--config CONFIG_FILE[,CONFIG_FILE2,...]", Array, "Custom configuration file"
+          c.option "force", "-f", "--force", "Overwrite a post if it already exists"
+          c.option "source", "-s", "--source SOURCE", "Custom source directory"
+
+          c.action do |args, options|
+            Jekyll::Commands::Publish.process(args, options)
+          end
+        end
+      end
+
+      def self.process(args = [], options = {})
+        params = PublishArgParser.new args, options
+        params.validate!
+
+        movement = DraftMovementInfo.new params
+
+        mover = DraftMover.new movement, params.force?, params.source
+        mover.move
+      end
+    end
+
+    class PublishArgParser < Compose::MovementArgParser
+      def resource_type
+        "draft"
+      end
+
+      def date
+        options["date"].nil? ? Date.today : Date.parse(options["date"])
+      end
+
+      def name
+        File.basename path
+      end
+    end
+
+    class DraftMovementInfo
+      attr_reader :params
+      def initialize(params)
+        @params = params
+      end
+
+      def from
+        params.path
+      end
+
+      def to
+        date_stamp = params.date.strftime "%Y-%m-%d"
+        "_posts/#{date_stamp}-#{params.name}"
+      end
+    end
+
+    class DraftMover < Compose::FileMover
+      def resource_type_from
+        "draft"
+      end
+      def resource_type_to
+        "post"
+      end
+    end
+  end
+end
