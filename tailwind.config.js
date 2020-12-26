bcca97bd159b448ba4d25cea61e3d7d504e2f311
Nick Tomlin <nick.tomlin@gmail.com>
2020-12-26 10:13:45 -0800

    chore: Remove backticks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    container: {
-      center: true,
-      padding: '1rem'
-    }
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem'
+      },
+      // I'm not a huge fan of the backticks in inline-code
+      // https://github.com/tailwindlabs/tailwindcss-typography/issues/18#issuecomment-733045571
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            }
+          }
+        }
+      },
+    },
   },
   variants: {
     extend: {},
